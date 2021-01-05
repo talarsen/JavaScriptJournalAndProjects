48b5a274149aa991abe7103f64e0f5e2b10de96e
outdoorcoder <talarsen74@gmail.com>
2021-01-05 12:55:56 -0800

    Add files via upload

--- donuts.js
+++ donuts.js
@@ -1,3 +1,7 @@
+/*Use the forEach() method to loop over the array and print out the following donut summaries using console.log.*/
+
+// donut represents a single element of donuts array
+//donut is an object, therefore you can access its properties using a dot notation
 var donuts = [
     { type: "Jelly", cost: 1.22 },
     { type: "Chocolate", cost: 2.45 },
@@ -5,6 +9,5 @@ var donuts = [
     { type: "Boston Cream", cost: 5.99 }
 ];
     donuts.forEach(function(donut) {
-    
         console.log(donut.type + " donuts costs $" + donut.cost + " each." );
     });
